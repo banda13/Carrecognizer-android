1a55b1afdcb217a6bf75654f41e8d8722a8cfdaf
Szabó András <szabag@gmail.com>
2019-04-05 15:25:29 +0200

    requesting permissions (camera + storage)

--- app/src/main/java/com/ai/deep/andy/carrecognizer/MainActivity.kt
+++ app/src/main/java/com/ai/deep/andy/carrecognizer/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.ai.deep.andy.carrecognizer
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
@@ -21,6 +23,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_main.view.*
 import java.io.File
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.provider.MediaStore
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
@@ -28,6 +31,8 @@ import android.util.Log
 import com.ai.deep.andy.carrecognizer.utils.Logger
 import java.io.IOException
 import android.os.AsyncTask.execute
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import com.ai.deep.andy.carrecognizer.ai.CarDetectorClassifier
 import com.ai.deep.andy.carrecognizer.ai.IClassifer
 import java.util.concurrent.Executors
@@ -40,11 +45,14 @@ private var classifier: CarDetectorClassifier? = null
 private val executor = Executors.newSingleThreadExecutor()
 
 private const val MODEL_PATH = "converted_model.tflite"
-private const val QUANT = false
+private const val QUANT = true
 private const val LABEL_PATH = "labels.txt"
 private const val INPUT_SIZE = 150
 
 private const val PICK_IMAGE_REQUEST = 1
+
+private const val CAMERA_REQUEST_CODE = 101
+private const val WRITE_STORAGE_REQUEST_CODE = 102
 
 class MainActivity : AppCompatActivity(), CameraFragment.OnCameraFragmentInteraction, ClassifyFragment.OnClassifyFragmentListener {
 
@@ -94,11 +102,84 @@ class MainActivity : AppCompatActivity(), CameraFragment.OnCameraFragmentInterac
         mSectionsPagerAdapter = SectionsPagerAdapter(supportFragmentManager)
 
         container.adapter = mSectionsPagerAdapter
-        container.currentItem = 1
+
+        requestPermissionIfNeeded()
 
         initTensorFlowAndLoadModel()
     }
 
+    private fun requestPermissionIfNeeded(){
+        val cameraPermission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+        val storagePermission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        if (cameraPermission != PackageManager.PERMISSION_GRANTED) {
+            Log.i(Logger.LOGTAG, "Permission to use camera not granted yet, requesting it from user")
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.CAMERA),
+                    CAMERA_REQUEST_CODE)
+        }
+        else if (storagePermission != PackageManager.PERMISSION_GRANTED) {
+            Log.i(Logger.LOGTAG, "Permission to write storage not granted yet, requesting it from user")
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                    WRITE_STORAGE_REQUEST_CODE)
+        }
+        else{
+            container.currentItem = 1
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            CAMERA_REQUEST_CODE -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Log.i(Logger.LOGTAG, "Camera Permission has been denied by user")
+                    Toast.makeText(this, "Using camera is necessary to take pictures of objects", Toast.LENGTH_SHORT).show()
+                    ActivityCompat.requestPermissions(this,
+                            arrayOf(Manifest.permission.CAMERA),
+                            CAMERA_REQUEST_CODE)
+                } else {
+                    Log.i(Logger.LOGTAG, "Camera Permission has been granted by user")
+                    val storagePermission = ContextCompat.checkSelfPermission(this,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    if (storagePermission != PackageManager.PERMISSION_GRANTED) {
+                        Log.i(Logger.LOGTAG, "Permission to write storage not granted yet, requesting it from user")
+                        ActivityCompat.requestPermissions(this,
+                                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                                WRITE_STORAGE_REQUEST_CODE)
+                    }
+                }
+            }
+            WRITE_STORAGE_REQUEST_CODE -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Log.i(Logger.LOGTAG, "Write storage permission has been denied by user")
+                    Toast.makeText(this, "Writing storage is necessary to save captured images to your gallery, if you click to the save button", Toast.LENGTH_SHORT).show()
+                    ActivityCompat.requestPermissions(this,
+                            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                            WRITE_STORAGE_REQUEST_CODE)
+                } else {
+                    Log.i(Logger.LOGTAG, "Writing storage Permission has been granted by user")
+                    val cameraPermission = ContextCompat.checkSelfPermission(this,
+                            Manifest.permission.CAMERA)
+                    if (cameraPermission != PackageManager.PERMISSION_GRANTED) {
+                        Log.i(Logger.LOGTAG, "Permission to use camera not granted yet, requesting it from user")
+                        ActivityCompat.requestPermissions(this,
+                                arrayOf(Manifest.permission.CAMERA),
+                                CAMERA_REQUEST_CODE)
+                    }
+                    else{
+                        container.currentItem = 1
+                    }
+                }
+            }
+            else -> {
+                Log.e(Logger.LOGTAG, "Unexpected permission request code captured")
+            }
+        }
+    }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
@@ -134,7 +215,7 @@ class MainActivity : AppCompatActivity(), CameraFragment.OnCameraFragmentInterac
     }
 
     private fun initTensorFlowAndLoadModel() {
-        executor.execute({
+        executor.execute {
             try {
                 Log.i(Logger.LOGTAG, "Initializing car detector classifier")
                 classifier = CarDetectorClassifier.create(
@@ -147,7 +228,7 @@ class MainActivity : AppCompatActivity(), CameraFragment.OnCameraFragmentInterac
             } catch (e: Exception) {
                 throw RuntimeException("Error initializing TensorFlow!", e)
             }
-        })
+        }
     }
 
     override fun onDestroy() {
